5dfbf7b890b43b5adac77b626ddf0692b5b52269
Alesia <alesia.litvinenko@gmail.com>
2023-03-28 22:27:04 +0300

    feat: change select input & file input

--- src/components/Forms/Inputs/InputFile.tsx
+++ src/components/Forms/Inputs/InputFile.tsx
@@ -1,41 +1,28 @@
-import { Component, createRef, RefObject } from 'react';
 import { ErrorMessage } from '../ErrorMessage/ErrorMessage';
-import { FormKeys } from '../../../const/validate-form-keys';
 import styles from './Input.module.scss';
+import { errorMessageTitile, FormKeys, titleForms } from '../../../const';
 
-export class InputFile extends Component<InputProps, InputState> {
-  fileInput: RefObject<HTMLInputElement>;
+export const InputFile: React.FC<InputProps> = ({ validate, onChange }) => {
+  const handlerChange = (fileInput: FileList | null) => {
+    const files = URL.createObjectURL((fileInput as FileList)[0] as Blob);
+    onChange(files ?? '');
+  };
 
-  constructor(props: InputProps) {
-    super(props);
-    this.fileInput = createRef();
-  }
+  return (
+    <div className={styles.item_input}>
+      <label htmlFor={FormKeys.FILE} className={styles.input_title}>
+        {titleForms.file}
+      </label>
+      <input
+        type="file"
+        className={`${styles.input_text} ${styles.input_file} ${!validate ? styles.error : ''}`}
+        accept={'.jpg,.jpeg,.png'}
+        onChange={(event) => handlerChange(event.target.files)}
+        id={FormKeys.FILE}
+        data-testid={FormKeys.FILE}
+      />
 
-  handlerChange() {
-    const files = URL.createObjectURL(this.fileInput.current?.files?.[0] as Blob);
-    this.props.onChange(files ?? '');
-  }
-
-  render() {
-    return (
-      <div className={styles.item_input}>
-        <label htmlFor={FormKeys.FILE} className={styles.input_title}>
-          Upload flag
-        </label>
-        <input
-          type="file"
-          className={`${styles.input_text} ${styles.input_file} ${
-            !this.props.validate ? styles.error : ''
-          }`}
-          ref={this.fileInput}
-          accept={'.jpg,.jpeg,.png'}
-          onChange={() => this.handlerChange()}
-          id={FormKeys.FILE}
-          data-testid={FormKeys.FILE}
-        />
-
-        <ErrorMessage validate={this.props.validate} errorMessage="upload the image" />
-      </div>
-    );
-  }
-}
+      <ErrorMessage validate={validate} errorMessage={errorMessageTitile.file} />
+    </div>
+  );
+};
--- src/const/error-message.ts
+++ src/const/error-message.ts
@@ -2,10 +2,10 @@ import { FormKeys } from './validate-form-keys';
 
 export const errorMessageTitile: Record<FormKeys, string> = {
   [FormKeys.TITLE]: 'the name must be more than 2 characters',
-  [FormKeys.DATE]: 'Flag creation date',
-  [FormKeys.FILE]: 'Upload flag',
+  [FormKeys.DATE]: 'the flag could not be created in the future',
+  [FormKeys.FILE]: 'upload the image',
   [FormKeys.SELECT]: 'select values',
   [FormKeys.CHECK]: 'Show you name',
-  [FormKeys.RADIO]: 'Is the official flag',
+  [FormKeys.RADIO]: 'we cannot make a choice for you, choose',
   [FormKeys.DECSRIPTIONS]: 'describe a little more (more than 10 characters)',
 };
